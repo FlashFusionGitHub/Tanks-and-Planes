ace08a1b944cd51e5122db72f54d67208eb100ac
Rowen <rowen.govender93@gmail.com>
2018-07-04 10:42:14 +0930

    Working on a tank selector script.

--- Assets/Scripts/TankManager.cs
+++ Assets/Scripts/TankManager.cs
@@ -1,16 +1,55 @@
-﻿using System.Collections;
+﻿using InControl;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TankManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    private InputDevice controller;
+
+    public List<TankActor> tanks;
+
+
+
+    private int TankIndex = 0;
+
+    private void Awake()
+    {
+        controller = InputManager.Devices[0];
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (controller.DPadRight.WasPressed)
+        {
+            if (TankIndex == tanks.Count - 1)
+                return;
+
+            TankIndex += 1;
+
+            SelectedTank(TankIndex);
+        }
+
+        if (controller.DPadLeft.WasPressed)
+        {
+            if (TankIndex == 0)
+                return;
+
+            TankIndex -= 1;
+
+            SelectedTank(TankIndex);
+        }
+    }
+
+    void SelectedTank(int index)
+    {
+        Debug.Log(tanks[index].name);
+    }
 }
