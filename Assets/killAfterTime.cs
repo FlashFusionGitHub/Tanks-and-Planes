bd00b8bb64386d6eeaac22303a8b4f599f2d0bdb
AIE\s162318 <mmatthewspatch1@gmail.com>
2018-07-24 16:02:36 +0930

    Added ParticleEffects to Prefab Folder as well Dependant Assets in the RAWARTASSETS Folder

---
+++ Assets/killAfterTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killAfterTime : MonoBehaviour {
+
+    [Tooltip("this value will be overwritten by particle duration if on is present")]
+    [SerializeField] private float m_timer;
+	// Use this for initialization
+	void Start () {
+        ParticleSystem PS = GetComponent<ParticleSystem>();
+        if (PS)
+            Destroy(gameObject, PS.main.duration);
+        else
+            Destroy(gameObject, m_timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
